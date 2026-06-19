5e8831a0f9e8f9f619112412325ca61826e58d19
BHUMIKSINGHDHAMI <dhamibhumiksingh@gmail.com>
2026-06-19 22:45:23 +0530

    Optimize bubbleSort with early exit condition
    Refactor bubbleSort to include a swapped flag for optimization.

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -3,12 +3,16 @@ using namespace std;
 
 void bubbleSort(vector<int>& arr, Metrics& m) {
     int n = arr.size();
-    for (int i = 0; i < n-1; i++)
+    for (int i = 0; i < n-1; i++){
+        bool swapped=false;
         for (int j = 0; j < n-i-1; j++) {
             m.comparisons++;
             if (arr[j] > arr[j+1]) {
                 swap(arr[j], arr[j+1]);
                 m.swaps++;
+                swapped=true;
             }
         }
+        if(!swapped) break;
+}
 }
