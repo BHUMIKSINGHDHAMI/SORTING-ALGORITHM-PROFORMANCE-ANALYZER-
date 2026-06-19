2019b690c0f0663c953fcdfc65509e808576341f
BHUMIKSINGHDHAMI <dhamibhumiksingh@gmail.com>
2026-06-19 22:47:20 +0530

    Refactor countSort function for clarity and efficiency

--- count_sort.cpp
+++ count_sort.cpp
@@ -5,31 +5,30 @@ using namespace std;
 void countSort(vector<int>& arr, Metrics &m) {
     if (arr.empty()) return;
 
+    int n = arr.size();
+
     int maxVal = *max_element(arr.begin(), arr.end());
     int minVal = *min_element(arr.begin(), arr.end());
+
     int range = maxVal - minVal + 1;
 
     vector<int> count(range, 0);
-    vector<int> output(arr.size());
+    vector<int> output(n);
 
     // Count frequency
-    for (int i = 0; i < arr.size(); i++) {
+    for (int i = 0; i < n; i++)
         count[arr[i] - minVal]++;
-        m.comparisons++;
-    }
 
-    // Prefix sum
-    for (int i = 1; i < range; i++) {
+    // Prefix sums
+    for (int i = 1; i < range; i++)
         count[i] += count[i - 1];
-    }
 
-    // Build output array
-    for (int i = arr.size() - 1; i >= 0; i--) {
+    // Stable placement
+    for (int i = n - 1; i >= 0; i--) {
         output[count[arr[i] - minVal] - 1] = arr[i];
         count[arr[i] - minVal]--;
-        m.swaps++;
+        m.swaps++;        
     }
 
-    // Copy back
     arr = output;
 }
